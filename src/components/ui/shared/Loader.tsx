d26693e64f30d96b737d48f38f0413c2cfe11da4
garpunk <garpunked@gmail.com>
2024-09-25 17:20:38 -0500

    first commit

---
+++ src/components/ui/shared/Loader.tsx
@@ -0,0 +1,12 @@
+const Loader = () => {
+  return (
+    <div className="flex-center w-full">
+      <img src="/assets/icons/loader.svg" alt="loader" width={24} height={24} />
+    </div>
+  );
+};
+
+export default Loader;
+
+// this icon currently rotates slower than I would like
+// find way to increase animation speed
---
+++ src/lib/appwrite/api.ts
@@ -0,0 +1,103 @@
+import { ID, Query } from 'appwrite';
+
+
+import { INewUser } from "@/types";
+import { account, appwriteConfig, avatars, databases } from './config';
+
+export async function createUserAccount(user: INewUser) {
+  try {
+     const newAccount = await account.create(
+        ID.unique(), 
+        user.email,
+        user.password,
+        user.name
+     );
+
+     if(!newAccount) throw Error;
+
+     const avatarUrl = avatars.getInitials(user.name);
+     
+     const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.username,
+      imageUrl: avatarUrl,
+
+     })
+
+     return newUser;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+
+}
+
+export async function saveUserToDB(user: {
+ accountId: string;
+ email: string;
+ name: string;
+ imageUrl: URL;
+ username?: string;
+
+
+
+}) {
+ try {
+  const newUser = await databases.createDocument(
+    appwriteConfig.databaseId,
+    appwriteConfig.userCollectionId,
+    ID.unique(),
+    user,
+  )
+
+  return newUser;
+ } catch (error) {
+  console.log(error);
+ }
+
+}
+
+export async function signInAccount(user:{ email: string; password: string; } ) {
+  try {
+    const session = await account.createEmailPasswordSession(user.email, user.password);
+// note: in appwrite v1.5, they use createEmailPasswordSession instead
+// of createEmailSession, this is a difference in the code that should be
+// noted for all future uses of this property in the tutorial.
+    return session;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getCurrentUser() {
+  try {
+    const currentAccount = await account.get();
+
+    if(!currentAccount) throw Error;
+
+    const currentUser = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal('accountId', currentAccount.$id)]
+    )
+
+    if(!currentUser) throw Error;
+
+    return currentUser.documents[0];
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function signOutAccount() {
+
+  try {
+    const session = await account.deleteSession("current");
+
+    return session;
+  } catch (error) {
+    
+  }
+}
